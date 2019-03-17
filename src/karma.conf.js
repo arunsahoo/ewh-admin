62b775663453e30b562d4526e77a2f580014adb3
Arun Kumar Sahoo <sahoo.kumar.arun@gmail.com>
2019-03-17 19:25:34 +0530

    modified to add customLaunchers in karma.conf.js

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -28,6 +28,22 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      HeadlessChromium: {
+        base: 'ChromiumHeadless',
+        flags: [
+          '--no-sandbox',
+          '--remote-debugging-port=9222',
+          '--enable-logging',
+          '--user-data-dir=./karma-chrome',
+          '--v=1',
+          '--disable-background-timer-throttling',
+          '--disable-renderer-backgrounding',
+          '--proxy-bypass-list=*',
+          '--proxy-server=\'direct://\''
+       ]
+      }
+    },
     singleRun: false
   });
 };
