2f3053052fcca165b294fc8b6ae392bf5c7005a6
Arun Kumar Sahoo <sahoo.kumar.arun@gmail.com>
2019-07-06 18:17:32 +0530

    configure browser test for circleci

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,7 +27,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeNoSandbox: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: true
   });
 };
