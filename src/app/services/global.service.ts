021aa34fd6ed79a502debdec8dd194687a6f7698
Arun Kumar Sahoo <sahoo.kumar.arun@gmail.com>
2020-07-25 20:36:03 +0530

    global service configuration for dev and production environment

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Injectable, enableProdMode } from '@angular/core';
 import Axios from 'axios';
+import { environment } from '../../environments/environment';
 import { UserLoginService } from './user-login.service';
 
 @Injectable({
@@ -8,8 +9,13 @@ import { UserLoginService } from './user-login.service';
 export class GlobalService {
 
   constructor(private userService: UserLoginService) {
-    // axios.defaults.baseURL = 'http://localhost/ewhapi/public/';
-    Axios.defaults.baseURL = 'http://127.0.0.1:1215/';
+    if (environment.production) {
+      enableProdMode();
+      Axios.defaults.baseURL = 'https://api.evokewebhosting.com/';
+    } else {
+      Axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+    }
+
     Axios.defaults.headers.common['X-Authorization'] = 'kT3ywIw376myEolJwPBzNs3fIC7ONjfxeEosXchYXO6L1JQYglEBJrk8L0u25fgn';
   }
 }
