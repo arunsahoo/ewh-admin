59ea10fc4314d0bc3f86593b5b13d9f823b4226a
Arun Kumar Sahoo <sahoo.kumar.arun@gmail.com>
2021-06-04 23:27:03 +0530

    modified to fix ui-switch changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -39,6 +40,7 @@ import { HeaderNavComponent } from './components/header-nav/header-nav.component
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     NgxSpinnerModule,
